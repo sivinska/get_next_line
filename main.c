93b2f0c802305f7a03fb12c06e01f4c3e306d1e6
Sarune IVINSKAITE <sivinska@e2r6p18.42.fr>
2018-02-20 17:31:10 +0100

    bettermain

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sivinska <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 19:00:42 by sivinska          #+#    #+#             */
-/*   Updated: 2018/02/14 15:13:00 by sivinska         ###   ########.fr       */
+/*   Updated: 2018/02/20 17:21:16 by sivinska         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,11 @@ int		main(int argc, char **argv)
 	if (argc || argv)
 		;
 	fd = open(argv[1], O_RDONLY);
-	while ((i = get_next_line(42, &line)) && (i > 0))
+	while ((i = get_next_line(fd, &line)) && (i > 0))
 	{
 		printf("%s\n", line);
 		free(line);
+		printf("return:%d\n", i);
 	}
 	printf("retourne:%d\n", i);
 	close(fd);
